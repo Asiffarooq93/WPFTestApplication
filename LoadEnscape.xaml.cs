3df86e4697960d7a81eeeaa681775eaf1b2e22ac
AsifFarooq93 <asif.farooq93@gmail.com>
2022-09-10 12:52:52 +0500

    Add project files.

---
+++ LoadEnscape.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTestApplication
+{
+    /// <summary>
+    /// Interaction logic for LoadEnscape.xaml
+    /// </summary>
+    public partial class LoadEnscape : Window
+    {
+        public LoadEnscape()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+
+        }
+
+        private void ButtonRemoveName_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (lstNames.Items.Count != 0)
+            { 
+            
+                lstNames.Items.RemoveAt(lstNames.Items.Count - 1);
+            }
+        }
+    }
+}
